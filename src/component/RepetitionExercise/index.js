d37c9ffc25704fd85e4313fdcea2f6ce1268cad3
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-02-16 12:00:42 -0500

    Add files via upload

--- src/component/RepetitionExercise/index.js
+++ src/component/RepetitionExercise/index.js
@@ -1,1 +1,16 @@
+import "index.css"
+import React, { useState } from 'react';
 
+function App(){
+  let [count, changeCounter] = useState(0);
+
+  const exercise = document.getElementById('excerise');
+
+  return (
+    <div>
+      <h1>Exercise: {exercise} </h1>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Add</button>
+    </div>
+  );
+}
