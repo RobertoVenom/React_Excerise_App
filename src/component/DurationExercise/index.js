378e3969b2e202cd1f924a0d41282870045680a6
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-02-16 11:09:55 -0500

    Add files via upload

--- src/component/DurationExercise/index.js
+++ src/component/DurationExercise/index.js
@@ -1,1 +1,12 @@
+import "index.css"
 
+function App(){
+  return(
+    <div className="exercise">
+      <button onclick=""></button>
+      <button onclick=""></button>
+      <button onclick=""></button>
+      <button onclick=""></button>
+    </div>
+  )   
+}
