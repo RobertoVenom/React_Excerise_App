93520b98d5fed89950765914bcd4d7c1a65f5a65
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-02-16 12:00:07 -0500

    Add files via upload

---
+++ src/component/app.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import ReactDOM from "component/DurationExercise";
+import ReactDOM from "component/RepetitionExercise";
+import { useState } from "react";
+import 'app.css'
+
+function ButtonList() {
+  const [selectedButton, setSelectedButton] = useState(null); 
+
+  const handleClick= (e) => {
+    const buttonId = e.target.id;
+    setSelectedButton(buttonId);
+  };
+
+  return(
+    <div onClick={handleClick}>
+      <button id="PushUps">Push Ups</button>
+      <button id="Bicycling">Bicycling</button>
+      <button id="JumpingJacks">Jumping Jacks</button>
+      <button id="Running">Running</button>
+      <button id="SitUps">Sit Ups</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ready for this</h1>
+      <h2>Choose the Exercise: {excerise}</h2>
+      <ButtonList />
+    </div>
+  );
+}
+export default App;
