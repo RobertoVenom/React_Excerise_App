f9f94b66e4a9455003e106d345abcdd8dc772ceb
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-02-22 08:59:20 -0500

    Add files via upload

--- src/component/RunningExercise/index.js
+++ src/component/RunningExercise/index.js
@@ -1,1 +1,16 @@
+import 'index.css';
+import React, { useState } from 'react';
 
+function App(){
+  let [count, changeCounter] = useState(0);
+
+  const running = document.getElementById('excerise');
+
+  return (
+    <div className='App'>
+      <h2>Exercise: {running} </h2>
+        <p>Laps: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Add</button>
+    </div>
+  );
+}
